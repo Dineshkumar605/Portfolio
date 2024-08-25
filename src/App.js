9cecb704c249ac1fa9b109303405420d987ea2fb
Dineshkumar605 <119146982+Dineshkumar605@users.noreply.github.com>
2024-08-25 07:24:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import TechStack from './components/TechStack';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import OtherProjectCards from './components/OtherProjectCards';
 
 function App() {
   const mainRef = React.useRef(null);
